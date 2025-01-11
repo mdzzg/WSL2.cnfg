da8f0fd2382fe811ddc8c42e32421d93734e59c0
mdzzg <mdamarija@gmail.com>
2025-01-11 15:03:02 +0100

    modified the syns script with rsync command + changed the renormalize definition of the git add command

--- DT/cnfg_sync_bnt.sh
+++ DT/cnfg_sync_bnt.sh
@@ -1,27 +1,109 @@
+# #!/bin/bash
+
+# if [ ~/cnfg.bak/.bash_aliases_WSL2 -nt ~/.bash_aliases ]; then
+#     cp -u ~/cnfg.bak/.bash_aliases_bnt ~/.bash_aliases
+# else
+#     cp -u ~/.bash_aliases ~/cnfg.bak/.bash_aliases_bnt
+# fi
+
+# if [ ~/cnfg.bak/.bashrc_WSL2 -nt ~/.bashrc ]; then
+#     cp -u ~/cnfg.bak/.bashrc_bnt ~/.bashrc
+# else
+#     cp -u ~/.bashrc ~/cnfg.bak/.bashrc_bnt
+# fi
+
+# if [ ~/cnfg.bak/git/config -nt ~/.config/git/config ]; then
+#     cp -u ~/cnfg.bak/git/config ~/.config/git/config
+# else
+#     cp -u ~/.config/git/config ~/cnfg.bak/git/config
+# fi
+
+# if [ ~/cnfg.bak/git/ignore -nt ~/.config/git/ignore ]; then
+#     cp -u ~/cnfg.bak/git/ignore ~/.config/git/ignore
+# else
+#     cp -u ~/.config/git/ignore ~/cnfg.bak/git/ignore
+# fi
+
+# echo "Backup complete"
+
+# # ====================================================================================================
+
+# #!/bin/bash
+
+# # Directories
+# DIR1="$HOME/cnfg.bak"
+# DIR2="$HOME/cnfg.bak/git"
+# DIR3="$HOME/.config/git"
+
+# # Function to synchronize files
+# sync_files() {
+#     local src="$1"
+#     local dest="$2"
+
+#     if [ "$src" -nt "$dest" ]; then
+#         echo "Copying $src to $dest"
+#         cp -u "$src" "$dest"
+#     elif [ "$dest" -nt "$src" ]; then
+#         echo "Copying $dest to $src"
+#         cp -u "$dest" "$src"
+#     else
+#         echo "No changes needed for $src and $dest"
+#     fi
+# }
+
+# # Ensure directories exist
+# [ ! -d "$DIR1" ] && echo "Error: Directory $DIR1 does not exist" && exit 1
+# [ ! -d "$DIR2" ] && echo "Error: Directory $DIR2 does not exist" && exit 1
+# [ ! -d "$DIR3" ] && echo "Error: Directory $DIR3 does not exist" && exit 1
+
+# # Synchronize files
+# sync_files "$DIR1/.bash_aliases_WSL2" "$HOME/.bash_aliases"
+# sync_files "$DIR1/.bashrc_WSL2" "$HOME/.bashrc"
+# sync_files "$DIR2/config" "$DIR3/config"
+# sync_files "$DIR2/ignore" "$DIR3/ignore"
+
+# echo "Backup complete"
+
+# ====================================================================================================
+
 #!/bin/bash
 
-if [ ~/cnfg.bak/.bash_aliases_WSL2 -nt ~/.bash_aliases ]; then
-    cp -u ~/cnfg.bak/.bash_aliases_bnt ~/.bash_aliases
-else
-    cp -u ~/.bash_aliases ~/cnfg.bak/.bash_aliases_bnt
-fi
+# Directories
+DIR1="$HOME/cnfg.bak"
+DIR2="$HOME/.config/git"
+DIR3="$DIR1/git"
 
-if [ ~/cnfg.bak/.bashrc_WSL2 -nt ~/.bashrc ]; then
-    cp -u ~/cnfg.bak/.bashrc_bnt ~/.bashrc
-else
-    cp -u ~/.bashrc ~/cnfg.bak/.bashrc_bnt
-fi
+# File pairs for syncing
+declare -A files=(
+    ["$DIR1/.bash_aliases_bnt"]="$HOME/.bash_aliases"
+    ["$DIR1/.bashrc_bnt"]="$HOME/.bashrc"
+    ["$DIR3/config"]="$DIR2/config"
+    ["$DIR3/ignore"]="$DIR2/ignore"
+)
 
-if [ ~/cnfg.bak/git/config -nt ~/.config/git/config ]; then
-    cp -u ~/cnfg.bak/git/config ~/.config/git/config
-else
-    cp -u ~/.config/git/config ~/cnfg.bak/git/config
-fi
+# Function to synchronize files
+sync_files() {
+    local src="$1"
+    local dest="$2"
 
-if [ ~/cnfg.bak/git/ignore -nt ~/.config/git/ignore ]; then
-    cp -u ~/cnfg.bak/git/ignore ~/.config/git/ignore
-else
-    cp -u ~/.config/git/ignore ~/cnfg.bak/git/ignore
-fi
+    # Sync src to dest only if src is newer
+    if [ "$src" -nt "$dest" ]; then
+        echo "Copying $src -> $dest"
+        rsync -auv "$src" "$dest"
+    elif [ "$dest" -nt "$src" ]; then
+        echo "Copying $dest -> $src"
+        rsync -auv "$dest" "$src"
+    else
+        echo "Both files are up-to-date: $src and $dest"
+    fi
+}
 
-echo "Backup complete"
+# Loop over file pairs to sync them
+for src in "${!files[@]}"; do
+    dest="${files[$src]}"
+    sync_files "$src" "$dest"
+done
+
+echo "Backup complete."
+
+# ====================================================================================================
