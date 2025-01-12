2e733bd3525cda37c518c5075b9ecb8e4b9c54f7
mdzzg <mdamarija@gmail.com>
2025-01-12 23:34:09 +0100

    Automated backup on 25.01.12-23:34:09

--- DT/data_sync_Lat.sh
+++ DT/data_sync_Lat.sh
@@ -4,14 +4,19 @@ echo "Starting file synchronization tasks..."
 
 # Define source and destination directories
 DOCS_SRC="/mnt/c/Users/mario/Documents"
-DOCS_DEST="/mnt/d/Lat_back"
 WSL2_SRC="/home/madz"
+
+# Target directories on the D drive 
+DOCS_DEST="/mnt/d/Lat_back"
 WSL2_DEST="/mnt/d/WSL2"
 
+# Common rsync options
+OPTIONS="-aSvEu --delete --info=progress2 --timeout=5"
+
 # Synchronize the Documents folder
-rsync -aSvEu --delete --info=progress2 --exclude="Downloaded Installations" --exclude="PTI" "$DOCS_SRC/" "$DOCS_DEST/"
+rsync $OPTIONS --exclude="Downloaded Installations" --exclude="PTI" "$DOCS_SRC/" "$DOCS_DEST/"
 
 # Synchronize the WSL2 home directory
-rsync -aSvEu --delete --info=progress2 --exclude="miniforge3" --exclude=".vscode-server-insiders" --exclude="*Zone.Identifier" "$WSL2_SRC/" "$WSL2_DEST/"
+rsync $OPTIONS --exclude="miniforge3" --exclude=".vscode-server-insiders" --exclude="*Zone.Identifier" "$WSL2_SRC/" "$WSL2_DEST/"
 
 echo "File synchronization tasks completed."
--- DT/data_sync_Len.sh
+++ DT/data_sync_Len.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
+# #!/bin/bash
 
-echo "Starting file synchronization tasks..."
+# echo "Starting file synchronization tasks..."
 
-# Define source and destination directories
-DOCS_SRC="/mnt/c/Users/mario/Documents"
-DOCS_DEST="/mnt/d/Lat_back"
-WSL2_SRC="/home/madz"
-WSL2_DEST="/mnt/d/WSL2"
+# # Define source and destination directories
+# DOCS_SRC="/mnt/c/Users/mario/Documents"
+# DOCS_DEST="/mnt/d/Lat_back"
+# WSL2_SRC="/home/madz"
+# WSL2_DEST="/mnt/d/WSL2"
 
-# Synchronize the Documents folder
-rsync -aSvEu --delete --info=progress2 --exclude="Downloaded Installations" --exclude="PTI" "$DOCS_SRC/" "$DOCS_DEST/"
+# # Synchronize the Documents folder
+# rsync -aSvEu --delete --info=progress2 --exclude="Downloaded Installations" --exclude="PTI" "$DOCS_SRC/" "$DOCS_DEST/"
 
-# Synchronize the WSL2 home directory
-rsync -aSvEu --delete --info=progress2 --exclude="miniforge3" --exclude=".vscode-server-insiders" --exclude="*Zone.Identifier" "$WSL2_SRC/" "$WSL2_DEST/"
+# # Synchronize the WSL2 home directory
+# rsync -aSvEu --delete --info=progress2 --exclude="miniforge3" --exclude=".vscode-server-insiders" --exclude="*Zone.Identifier" "$WSL2_SRC/" "$WSL2_DEST/"
 
-echo "File synchronization tasks completed."
+# echo "File synchronization tasks completed."
 
 # ==============================================================================================================================================
 
@@ -31,7 +31,7 @@ DEST1="/mnt/tsclient/D/strucno/"
 DEST2="/mnt/tsclient/D/privatno/"
 
 # Common rsync options
-OPTIONS="-av --delete --progress --ignore-errors --timeout=5"
+OPTIONS="-aSvEu --delete --info=progress2 --timeout=5"
 
 # Sync command for SRC1 to DEST1
 echo "Synchronizing $SRC1 to $DEST1"
