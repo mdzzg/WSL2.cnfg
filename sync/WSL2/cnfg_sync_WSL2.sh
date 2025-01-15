f8bccffc3715d9a76822700c8a18b773f1bb9302
mdzzg <mdamarija@gmail.com>
2025-01-15 12:38:48 +0100

    Automated backup on 25.01.15-12:38:48

--- sync/WSL2/cnfg_sync_WSL2.sh
+++ sync/WSL2/cnfg_sync_WSL2.sh
@@ -44,7 +44,7 @@ declare -A files=(
     ["$DIR3/.bash_aliases"]="$HOME/.bash_aliases"
     ["$DIR3/.bashrc"]="$HOME/.bashrc"
     ["$DIR3/.inputrc"]="$HOME/.inputrc"
-    ["$DIR3/.bash_history"]="$HOME/.bash_history"
+    # ["$DIR3/.bash_history"]="$HOME/.bash_history"
     ["$DIR4/config"]="$DIR5/config"
     ["$DIR4/ignore"]="$DIR5/ignore"
 )
