cf9d77a33c0bcb673648a3babbbced399c46e112
mdzzg <mdamarija@gmail.com>
2025-01-20 11:02:33 +0100

    Automated backup on 25.01.20-11:02:33

--- sync/WSL2/cnfg_sync_WSL2.sh
+++ sync/WSL2/cnfg_sync_WSL2.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 
 # Directories
-DIR1="$HOME/cnfg.bak"
-DIR2="$DIR1/sync"
-DIR3="$DIR2/WSL2"
-DIR4="$DIR2/git"
-DIR5="$HOME/.config/git"
+DIR01="$HOME/cnfg.bak"
+DIR02="$DIR01/sync"
+DIR03="$DIR02/WSL"
+DIR04="$DIR02/cmmn"
+DIR05="$DIR04/git"
+DIR06="$HOME/.config/git"
 
 # File pairs for syncing
 declare -A files=(
-    ["$DIR3/.bash_aliases"]="$HOME/.bash_aliases"
-    ["$DIR3/.bashrc"]="$HOME/.bashrc"
-    ["$DIR3/.inputrc"]="$HOME/.inputrc"
-    ["$DIR4/config"]="$DIR5/config"
-    ["$DIR4/ignore"]="$DIR5/ignore"
+    ["$DIR03/.bash_aliases"]="$HOME/.bash_aliases"
+    ["$DIR03/.bashrc"]="$HOME/.bashrc"
+    ["$DIR04/.inputrc"]="$HOME/.inputrc"
+    ["$DIR04/.dircolors"]="$HOME/.dircolors"
+    ["$DIR05/config"]="$DIR06/config"
+    ["$DIR05/ignore"]="$DIR06/ignore"
 )
 
 # Function to synchronize files
 sync_files() {
-    local src="$1"
-    local dst="$2"
+    local src_1="$1"
+    local src_2="$2"
 
     # Sync src to dest only if src is newer
-    if [ "$src" -nt "$dst" ]; then
-        echo "The $src is newer than the $dst"
-        echo "Copying $src -> $dst"
-        rsync -auv "$src" "$dst"
-    elif [ "$src" -ot "$dst" ]; then
-        echo "The $dst is newer than the $src"
-        echo "Copying $dst -> $src"
-        rsync -auv "$dst" "$src"
+    if [ "$src_1" -nt "$src_2" ]; then
+        echo "The $src_1 is newer than the $src_2"
+        echo "Copying $src_1 -> $src_2"
+        rsync -auv "$src_1" "$src_2"
+    elif [ "$src_1" -ot "$src_2" ]; then
+        echo "The $src_2 is newer than the $src_1"
+        echo "Copying $src_2 -> $src_1"
+        rsync -auv "$src_2" "$src_1"
     else
-        echo "Both files are in sync: $src and $dst"
+        echo "Both files are in sync: $src_1 and $src_2"
     fi
 }
 
 # Loop over file pairs to sync them
-for src in "${!files[@]}"; do
-    dst="${files[$src]}"
-    sync_files "$src" "$dst"
+for src_1 in "${!files[@]}"; do
+    src_2="${files[$src_1]}"
+    sync_files "$src_1" "$src_2"
 done
 
 cd "$DIR1" || exit
