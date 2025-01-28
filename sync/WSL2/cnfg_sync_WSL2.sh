edd7b2d1233d4b5ce0f8834e1a052c60d2c31df2
mdzzg <mdamarija@gmail.com>
2025-01-28 16:27:38 +0100

    Automated backup on 25.01.28-16:27:38

--- sync/WSL2/cnfg_sync_WSL2.sh
+++ sync/WSL2/cnfg_sync_WSL2.sh
@@ -50,7 +50,7 @@ git ad
 git ct "Automated backup on $(date +%y.%m.%d-%H:%M:%S)"
 git u
 
-echo "Backup complete and pushed to GitHub."
+# echo "Backup complete and pushed to GitHub."
 
 # ====================================================================================================
 
