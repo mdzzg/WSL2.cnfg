182d6d7f88b9402641ec3e23f5945f0f6b051ff3
mdzzg <mdamarija@gmail.com>
2025-02-01 17:57:01 +0100

    renaming subfolders

--- csync/WSL2/cnfg_sync_WSL2.sh
+++ csync/WSL2/cnfg_sync_WSL2.sh
@@ -20,15 +20,11 @@
 
 # Directories
 DIR01="$HOME/cnfg.bak"
-DIR02="$DIR01/sync"
-DIR03="$DIR02/BNT"
+DIR02="$DIR01/csync"
+DIR03="$DIR02/WSL2"
 DIR04="$DIR02/cmmn"
 DIR05="$DIR04/git"
 DIR06="$HOME/.config/git"
-# DIR07="$DIR01/fstb"
-# DIR08="/etc"
-# DIR09="$DIR1/crtb"
-# DIR10="/var/spool/cron/crontabs"
 
 # File pairs for syncing
 declare -A files=(
@@ -38,7 +34,6 @@ declare -A files=(
     ["$DIR04/.dircolors"]="$HOME/.dircolors"
     ["$DIR05/config"]="$DIR06/config"
     ["$DIR05/ignore"]="$DIR06/ignore"
-    # ["$DIR07/fstab.P5550#2"]="$DIR08/fstab"
 )
 
 # Function to synchronize files
@@ -55,6 +50,7 @@ sync_files() {
         echo "The $src_2 is newer than the $src_1"
         echo "Copying $src_2 -> $src_1"
         rsync -auv "$src_2" "$src_1"
+#	touch -r "$src_1" "$src_2"
     else
         echo "Both files are in sync: $src_1 and $src_2"
     fi
@@ -75,5 +71,37 @@ git ct "Automated backup on $(date +%y.%m.%d_%H:%M:%S)"
 git u
 
 # echo "Backup complete and pushed to GitHub."
+
+# ====================================================================================================
+
+# DIR1="$HOME/cnfg.bak"
+# DIR2="$HOME/.config/git"
+# DIR3="$DIR1/git"
+
+# if [ "$DIR1/.bash_aliases_WSL2" -nt "$HOME/.bash_aliases" ]; then
+#     cp -u "$DIR1/.bash_aliases_WSL2" "$HOME/.bash_aliases"
+# else
+#     cp -u "$HOME/.bash_aliases" "$DIR1/.bash_aliases_WSL2"
+# fi
+
+# if [ "$DIR1/.bashrc_WSL2" -nt "$HOME/.bashrc" ]; then
+#     cp -u "$DIR1/.bashrc_WSL2" "$HOME/.bashrc"
+# else
+#     cp -u "$HOME/.bashrc" "$DIR1/.bashrc_WSL2"
+# fi
+
+# if [ "$DIR3/config" -nt "$DIR2/config" ]; then
+#     cp -u "$DIR3/config" "$DIR2/config"
+# else
+#     cp -u "$DIR2/config" "$DIR3/config"
+# fi
+
+# if [ "$DIR3/ignore" -nt "$DIR2/ignore" ]; then
+#     cp -uv "$DIR3/ignore" "$DIR2/ignore"
+# else
+#     cp -uv "$DIR2/ignore" "$DIR3/ignore"
+# fi
+
+# echo "Backup complete"
 
 # ====================================================================================================
