d7348ed99fec348d61c9afcb0a71efe4c4f4fe65
mdzzg <mdamarija@gmail.com>
2025-02-03 11:30:02 +0100

    Automated backup on 25.02.03_11:30:02

--- dsync/backup_script/backup_Lat_E7450.sh
+++ dsync/backup_script/backup_Lat_E7450.sh
@@ -5,7 +5,7 @@ echo Starting file synchronization tasks...
 robocopy "C:\Users\mario\Documents" "D:\Lat_back" /PURGE /E /DCOPY:T /COPY:DAT /R:2 /W:5 /NDL /NFL /ETA /XD "Downloaded Installations" "PTI" "com.pieces.*" /XF "*.rdp" "desktop.ini"
 
 :: Lat_E7450 Command 2: Copy WSL2 home directory
-robocopy "\\wsl$\Ubuntu-22.04\home\madz" "D:\WSL2" /PURGE /E /DCOPY:T /COPY:DAT /R:2 /W:5 /NDL /NFL /ETA /XD "miniforge3" ".vscode-server-insiders" "snap" /XF "*Zone.Identifier"
+robocopy "\\wsl$\Ubuntu-22.04\home\madz" "D:\WSL2" /PURGE /E /DCOPY:T /COPY:DAT /R:2 /W:5 /NDL /NFL /ETA  /XD "miniforge3" ".vscode-server-insiders" "snap" /XF "*Zone.Identifier"
 
 echo Lat_7450 file synchronization tasks completed.
 pause
